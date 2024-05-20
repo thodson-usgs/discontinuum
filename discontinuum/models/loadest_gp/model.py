ef8893871c83db1f9f05000d5d1b5a3f9d67bd25
thodson <thodson@usgs.gov>
2024-05-20 09:09:15 -0500

    Add covariates to loadest_gp

--- discontinuum/models/loadest_gp/model.py
+++ discontinuum/models/loadest_gp/model.py
@@ -1,4 +1,5 @@
 import pymc as pm
+import numpy as np
 
 from discontinuum.engines.pymc import MarginalGP
 from discontinuum.data_manager import DataManager
@@ -25,6 +26,11 @@ class LoadestGP(MarginalGP, PlotMixin):
 
     def build_model(self, X, y) -> pm.Model:
         """ """
+        n_covariates = X.shape[1]
+        dims = np.arange(n_covariates)
+        time_dim = [dims[0]]
+        cov_dims = dims[1:]
+
         with pm.Model() as model:
             # seasonal trend
             eta_per = pm.HalfNormal("eta_per", sigma=1, initval=1)  # was 2
@@ -35,8 +41,8 @@ class LoadestGP(MarginalGP, PlotMixin):
 
             cov_seasonal = (
                 eta_per**2
-                * pm.gp.cov.Periodic(2, period, ls_psmooth, active_dims=[0])
-                * pm.gp.cov.Matern52(2, ls_pdecay, active_dims=[0])
+                * pm.gp.cov.Periodic(2, period, ls_psmooth, active_dims=time_dim)
+                * pm.gp.cov.Matern52(2, ls_pdecay, active_dims=time_dim)
             )
             gp_seasonal = pm.gp.Marginal(cov_func=cov_seasonal)
 
@@ -46,27 +52,28 @@ class LoadestGP(MarginalGP, PlotMixin):
                 "eta_trend", scale=1.5
             )  # Exponential might to limit outliers dictating a trend
             ls_trend = pm.LogNormal("ls_trend", mu=2, sigma=1)
-            cov_trend = eta_trend**2 * pm.gp.cov.ExpQuad(2, ls_trend, active_dims=[0])
+            cov_trend = eta_trend**2 * pm.gp.cov.ExpQuad(2, ls_trend, active_dims=time_dim)
             gp_trend = pm.gp.Marginal(cov_func=cov_trend)
 
             # covariate trend
+            # could include time with a different prior on ls
             eta_covariates = pm.HalfNormal("eta_covariates", sigma=2)
             ls_covariates = pm.LogNormal("ls_covariates", mu=-1.1, sigma=1, initval=0.5)
             cov_covariates = eta_covariates**2 \
-                * pm.gp.cov.ExpQuad(2, ls=ls_covariates, active_dims=[1])
+                * pm.gp.cov.ExpQuad(2, ls=ls_covariates, active_dims=cov_dims)
             gp_covariates = pm.gp.Marginal(cov_func=cov_covariates)
 
             # residual trend
-            # potentially merge with covariate model
             eta_res = pm.Exponential("eta_res", scale=0.2)
             ls_res = pm.LogNormal("ls_res", mu=-1.1, sigma=1, shape=2)
-            cov_res = eta_res**2 * pm.gp.cov.ExpQuad(2, ls_res, active_dims=[0, 1])
+            cov_res = eta_res**2 * pm.gp.cov.ExpQuad(2, ls_res, active_dims=dims)
             gp_res = pm.gp.Marginal(cov_func=cov_res)
 
             gp = gp_trend + gp_seasonal + gp_covariates + gp_res
 
             # noise_model
-            # setting a small value ensures cov is positive definite
+            # set to a small value to ensure cov is positive definite
+            # or use a HalfNormal prior
             cov_noise = pm.gp.cov.WhiteNoise(0.1)
 
             y_ = gp.marginal_likelihood("y", X=X, y=y, sigma=cov_noise)
