9f45ab5adf5489b0b6e69429a31b027902a0a4bb
thodson <thodson@usgs.gov>
2024-05-22 16:19:33 -0500

    Add time_substitution counterfactual

--- discontinuum/utils.py
+++ discontinuum/utils.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pandas as pd
+import numpy as np
 
 if TYPE_CHECKING:
     from xarray import Dataset
@@ -30,3 +31,37 @@ def aggregate_to_daily(ds: Dataset) -> Dataset:
     daily = daily.rename({"date": "time"})
 
     return daily
+
+
+def time_substitution(ds: Dataset, interval: slice) -> Dataset:
+    """Create a counterfactual by substituting data from an interval.
+
+    Takes the data interval and repreats it to fill the whole dataset.
+    For the dataset [1, 2, 3, 4, 5], `substitution(ds, slice(0, 0))`
+    returns [1, 1, 1, 1, 1].
+
+    Parameters
+    ----------
+    ds : Dataset
+        Data to substitute.
+    interval : slice
+        Interval to substitute.
+
+    Returns
+    -------
+    Dataset
+        Data with substituted data.
+    """
+    out = ds.copy()
+    ds_slice = ds.sel(time=interval)
+
+    n_d = ds['time'].shape[0]
+    n_s = ds_slice['time'].shape[0]
+
+    rep = np.ceil(n_d / n_s)
+
+    # for variable in dataset tile the slice and keep the first n_d values
+    for var in ds.data_vars:
+        out[var].values = np.tile(ds_slice[var].values, int(rep))[:n_d]
+
+    return out
