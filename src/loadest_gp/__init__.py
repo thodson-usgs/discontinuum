52213e38e7e41eda2297a60f7e692f512e3c2b9c
thodson <thodson@usgs.gov>
2024-08-23 10:22:46 -0500

    Fix pymc dependency

--- src/loadest_gp/__init__.py
+++ src/loadest_gp/__init__.py
@@ -1,14 +1,34 @@
-from loadest_gp.models.gpytorch import (
-    LoadestGPMarginalGPyTorch,
-)
-from loadest_gp.models.pymc import (
-    LoadestGPMarginalPyMC,
-)
+import sys
+import warnings
 
-__all__ = (
-    # Sub-packages
-    # "providers",
-    # Top-level functions
-    "LoadestGPMarginalPyMC",
+__all_gpytorch__ = [
+    # Sub-packages in models.gpytorch
     "LoadestGPMarginalGPyTorch",
-)
+]
+
+__all_pymc__ = [
+    # PyMC sub-packages in models.pymc
+    "LoadestGPMarginalPyMC",
+]
+
+for name in __all_gpytorch__:
+    exec(f"from .models.gpytorch import {name}")
+
+__all__ = __all_gpytorch__.copy()
+
+
+# check if pymc is installed, then import the sub-packages
+try:
+    import pymc
+    __all__.extend(__all_pymc__)
+
+    for name in __all_pymc__:
+        exec(f"from .models.pymc import {name}")
+
+except ModuleNotFoundError:
+    # if pymc is not installed, raise a warning
+    warnings.warn(
+        f"`pymc` is not installed: {', '.join(__all_pymc__)}"
+        " will not be available.",
+        UserWarning,
+    )
